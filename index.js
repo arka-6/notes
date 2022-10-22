605c7268b2530acb1ea7ba974aa5deb6bf3eaee0
Lumenobit <lumenobit@gmail.com>
2022-10-22 13:58:03 +0530

    vercel changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 // Note: If the file name is index.js we dont need to mention the file name.
 // Instead of ./api/index, we can write ./api
-const apiRouter = require('./api');
+const apiRouter = require('./src/api');
 
 const server = express();
 const port = 4000 || process.env.PORT;
@@ -14,7 +14,7 @@ server.use(bodyParser.json());
 server.use('/api', apiRouter);
 
 // __dirname gets the current directory of the file (index.js in this case).
-server.use('/', express.static(path.join(__dirname, 'public')));
+server.use('/', express.static(path.join(__dirname, 'src/public')));
 
 server.listen(port, () => {
     console.log(`Server listening to port ${port}`);
